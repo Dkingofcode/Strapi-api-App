5f28542684c8a1b015558f5ef8e224af0fae11d3
Dkingofcode <doladepo128@gmail.com>
2023-04-04 11:22:36 +0100

    Finished the hide submenu logic

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { useGlobalContext } from './context';
 import { FaBars } from 'react-icons/fa';
 import NavLinks from './NavLinks';
+//import { handle } from 'express/lib/application';
+
+
 
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+
+ const handleSubmenu = (e) => {
+     console.log(e.target);
+     if(!e.target.classList.contains("nav-link")){
+        setPageId(null);
+     }
+  }
 
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
     <div className='nav-center'>
      <h3 className='logo'>strapi</h3>
       <button className='toggle-btn' onClick={openSidebar}>
